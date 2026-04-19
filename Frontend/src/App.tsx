54c82e88e4049a2a3ee09cbb242e818840124328
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2026-04-19 16:12:04 +0530

    feat: initialize frontend project structure with routing, React Query, and Auth/Chat pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,16 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth/Auth";
+import Chat from "./pages/Chat/Chat";
 
+function App() {
   return (
     <>
-      <h1>Zentalk</h1>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/pages/Auth/Auth.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Auth = () => {
+    return (
+        <div>
+            <h1>Auth</h1>
+        </div>
+    )
+}
+
+export default Auth;
